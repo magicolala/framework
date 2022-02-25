a7534a59a117f5722bc737e8a9981906cad5e8ef
Cédric Oloa <oloa.cedric.bts@gmail.com>
2022-02-25 10:59:39 +0100

    controller resolver

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
 use Symfony\Component\Routing\RequestContext;
@@ -16,24 +17,12 @@ $context = new RequestContext();
 $context->fromRequest($request);
 
 $urlMatcher = new UrlMatcher($routes, $context);
+$controllerResolver = new ControllerResolver();
 
 try {
     $result = $urlMatcher->match($request->getPathInfo());
-
-    $className = substr(
-        $result["_controller"],
-        0,
-        strpos($result["_controller"], '@')
-    );
-
-    $methodName = substr(
-        $result["_controller"],
-        strpos($result["_controller"], '@') + 1
-    );
-
-    $controller = [new $className, $methodName];
-
     $request->attributes->add($result);
+    $controller = $controllerResolver->getController($request);
     $response = call_user_func($controller, $request);
 } catch (ResourceNotFoundException $e) {
     $response = new Response("La page demandée n'existe pas. " . $e->getMessage(), 404);
