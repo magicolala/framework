3c3b2b7f7cf52ec7c3116b013f4fc1fd1398b24b
Cédric Oloa <oloa.cedric.bts@gmail.com>
2022-02-25 14:22:29 +0100

    simplex

---
+++ framework/Simplex.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Framework;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
+use Symfony\Component\Routing\Exception\ResourceNotFoundException;
+use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\Routing\RequestContext;
+
+class Simplex
+{
+    public function handle(Request $request)
+    {
+        $routes = require __DIR__ . "/../src/routes.php";
+
+        $context = new RequestContext();
+        $context->fromRequest($request);
+
+        $urlMatcher = new UrlMatcher($routes, $context);
+        $controllerResolver = new ControllerResolver();
+        $argumentResolver = new ArgumentResolver();
+
+        try {
+            $request->attributes->add($urlMatcher->match($request->getPathInfo()));
+            $controller = $controllerResolver->getController($request);
+            $arguments = $argumentResolver->getArguments($request, $controller);
+            $response = call_user_func_array($controller, $arguments);
+        } catch (ResourceNotFoundException $e) {
+            $response = new Response("La page demandée n'existe pas. " . $e->getMessage(), 404);
+        } catch (Exception $e) {
+            $response = new Response("Une erreur est survenue. " . $e->getMessage(), 500);
+        }
+
+        return $response;
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,35 +1,14 @@
 <?php
 
+use Framework\Simplex;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Controller\ControllerResolver;
-use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
-use Symfony\Component\Routing\Exception\ResourceNotFoundException;
-use Symfony\Component\Routing\Matcher\UrlMatcher;
-use Symfony\Component\Routing\RequestContext;
 
 require_once __DIR__ . "/../vendor/autoload.php";
 
 $request = Request::createFromGlobals();
 
-$routes = require __DIR__ . "/../src/routes.php";
+$framework = new Simplex();
 
-$context = new RequestContext();
-$context->fromRequest($request);
-
-$urlMatcher = new UrlMatcher($routes, $context);
-$controllerResolver = new ControllerResolver();
-$argumentResolver = new ArgumentResolver();
-
-try {
-    $request->attributes->add($urlMatcher->match($request->getPathInfo()));
-    $controller = $controllerResolver->getController($request);
-    $arguments = $argumentResolver->getArguments($request, $controller);
-    $response = call_user_func_array($controller, $arguments);
-} catch (ResourceNotFoundException $e) {
-    $response = new Response("La page demandée n'existe pas. " . $e->getMessage(), 404);
-} catch (Exception $e) {
-    $response = new Response("Une erreur est survenue. " . $e->getMessage(), 500);
-}
+$response = $framework->handle($request);
 
 $response->send();
